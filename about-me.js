03ff8d28ffa5901c19ebe9a3dd52b2482a1218c2
Letto228 <learnjs@macbook-pro-egor.local>
2023-02-09 23:43:41 +0300

    feat: description for app module

---
+++ about-me.js
@@ -0,0 +1,4 @@
+// Hello everyone!
+// My name is Stanislav and I would like to take an advanced development course on Angular
+// I am currently working in a small company as a junior, so I wanted to boost my skill
+// I think I will succeed, because there is a mood and a good teacher
