5f8b77c4c8da1bee07ce984682d9bd79fb0a4e6d
BoikoS <s.boyko@it-alnc.ru>
2023-02-14 23:46:23 +0300

    feat: add card component

--- src/app/core/sidenav/sidenav.module.ts
+++ src/app/core/sidenav/sidenav.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { SidenavComponent } from './sidenav.component';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
+import { CardModule } from 'src/app/pages/products-list/card/card.module';
 
 @NgModule({
 	declarations: [SidenavComponent],
-	imports: [CommonModule, MatSidenavModule, MatButtonModule],
+	imports: [CommonModule, MatSidenavModule, MatButtonModule, CardModule],
 	exports: [SidenavComponent],
 })
 export class SidenavModule {}
