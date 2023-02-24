650350217ab6e3cf0f56941437390acbb1b36086
KEA <kjanni22@gmail.com>
2023-02-24 16:07:51 +0300

    feat: add empty directive appScrollWithLoading

---
+++ src/app/shared/app-scroll-with-loading/app-scroll-with-loading.directive.spec.ts
@@ -0,0 +1,8 @@
+import { AppScrollWithLoadingDirective } from './app-scroll-with-loading.directive';
+
+describe('AppScrollWithLoadingDirective', () => {
+	it('should create an instance', () => {
+		const directive = new AppScrollWithLoadingDirective();
+		expect(directive).toBeTruthy();
+	});
+});
---
+++ src/app/shared/app-scroll-with-loading/app-scroll-with-loading.directive.ts
@@ -0,0 +1,8 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+	selector: '[appAppScrollWithLoading]',
+})
+export class AppScrollWithLoadingDirective {
+	//constructor() { }
+}
