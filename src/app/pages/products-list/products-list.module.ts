14d8cd5a3591d62e01a39acd73d6428c2fb117ae
Letto228 <learnjs@MacBook-Pro-Egor.local>
2023-02-27 19:18:23 +0300

    feat: add scroll with loading directive

--- src/app/pages/products-list/products-list.module.ts
+++ src/app/pages/products-list/products-list.module.ts
@@ -5,10 +5,11 @@ import { CardModule } from './card/card.module';
 import { DumpNgIfModule } from '../../shared/dump-ng-if/dump-ng-if.module';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { LetModule } from '../../shared/let/let.module';
+import { ScrollWithLoadingModule } from '../../shared/scroll-with-loading/scroll-with-loading.module';
 
 @NgModule({
 	declarations: [ProductsListComponent],
-	imports: [CommonModule, CardModule, DumpNgIfModule, MatProgressSpinnerModule, LetModule],
+	imports: [CommonModule, CardModule, DumpNgIfModule, MatProgressSpinnerModule, LetModule, ScrollWithLoadingModule],
 	exports: [ProductsListComponent],
 })
 export class ProductsListModule {}
