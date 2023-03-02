74959903f0c5a8b2ba89c36b95939af0124c1265
AVoitsenya <vojtsenyaas@it2g.ru>
2023-03-02 11:02:33 +0300

    feat: pagination

--- src/app/pages/products-list/card/card.module.ts
+++ src/app/pages/products-list/card/card.module.ts
@@ -5,10 +5,11 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { CarouselModule } from '../../../shared/carousel/carousel.module';
+import { PaginationModule } from '../../../shared/pagination/pagination.module';
 
 @NgModule({
 	declarations: [CardComponent],
-	imports: [CommonModule, MatCardModule, MatButtonModule, MatIconModule, CarouselModule],
+	imports: [CommonModule, MatCardModule, MatButtonModule, MatIconModule, CarouselModule, PaginationModule],
 	exports: [CardComponent],
 })
 export class CardModule {}
--- src/app/pages/products-list/products-list.component.ts
+++ src/app/pages/products-list/products-list.component.ts
@@ -1,19 +1,7 @@
-import {
-	AfterViewInit,
-	ChangeDetectionStrategy,
-	ChangeDetectorRef,
-	Component,
-	ContentChildren,
-	NgZone,
-	OnInit,
-	QueryList,
-	ViewChildren,
-} from '@angular/core';
-import { of } from 'rxjs';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, NgZone, OnInit } from '@angular/core';
 import { IProduct } from '../../shared/products/product.interface';
 import { productsMock } from '../../shared/products/products.mock';
 import { LoadDirection } from '../../shared/scroll-with-loading/load-direction.const';
-import { CardComponent } from './card/card.component';
 
 @Component({
 	selector: 'app-products-list',
@@ -23,18 +11,15 @@ import { CardComponent } from './card/card.component';
 })
 export class ProductsListComponent implements OnInit {
 	products: IProduct[] | undefined = undefined;
+	perPage = 3;
 
-	constructor(private readonly changeDetectorRef: ChangeDetectorRef, private readonly ngZone: NgZone) {}
+	constructor(private readonly changeDetectorRef: ChangeDetectorRef) {}
 
 	ngOnInit() {
 		setTimeout(() => {
 			this.products = productsMock;
 			this.changeDetectorRef.markForCheck();
-		}, 3000);
-		setTimeout(() => {
-			this.products = productsMock.slice(2);
-			this.changeDetectorRef.markForCheck();
-		}, 6000);
+		}, 0);
 	}
 
 	getProducts(): IProduct[] | undefined {
