91b0976d37806e7c90c2399d96cdfab48a111acd
KEA <kjanni22@gmail.com>
2023-03-04 08:50:22 +0300

    feat: fix AppScrollWithLoadingDirective

--- src/app/shared/app-scroll-with-loading/app-scroll-with-loading.directive.ts
+++ src/app/shared/app-scroll-with-loading/app-scroll-with-loading.directive.ts
@@ -5,30 +5,31 @@ import { ScrollLoadingDirection } from './scroll-loading-direction.enum';
 	selector: '[appScrollWithLoading]',
 })
 export class AppScrollWithLoadingDirective {
+	private borderOffset = 150; //отступ
+
 	@Output()
 	scrollLoadingDirection = new EventEmitter<ScrollLoadingDirection | undefined>();
 
-	@HostListener('scroll', ['$event'])
-	onScroll(event: any) {
-		const target = event.target;
+	@HostListener('scroll', ['$event.target'])
+	onScroll(target: HTMLElement) {
 		const scrollTop = target.scrollTop; //высота невидимой, прокрученной в данный момент, части элемента
 		const clientHeight = target.clientHeight; //размер области внутри рамок элемента.
 		const scrollHeight = target.scrollHeight; // полная внутренняя высота, включая прокрученную область.
-		const borderOffset = scrollHeight / 10; //отступ 1/10 от окна
 
-		if (this.isNeedLoadBefore(borderOffset, scrollTop)) {
+		if (isNeedLoadBefore(this.borderOffset, scrollTop)) {
 			this.scrollLoadingDirection.emit(ScrollLoadingDirection.Before);
-		} else if (this.isNeedLoadAfter(borderOffset, scrollHeight, clientHeight, scrollTop)) {
+			return;
+		} else if (isNeedLoadAfter(this.borderOffset, scrollHeight, clientHeight, scrollTop)) {
 			this.scrollLoadingDirection.emit(ScrollLoadingDirection.After);
 		}
 	}
+}
 
-	isNeedLoadBefore(borderOffset: number, scrollTop: number): boolean {
-		return scrollTop <= borderOffset;
-	}
+function isNeedLoadBefore(borderOffset: number, scrollTop: number): boolean {
+	return scrollTop <= borderOffset;
+}
 
-	isNeedLoadAfter(borderOffset: number, scrollHeight: number, clientHeight: number, scrollTop: number): boolean {
-		const position = scrollHeight - scrollTop - clientHeight;
-		return position <= borderOffset;
-	}
+function isNeedLoadAfter(borderOffset: number, scrollHeight: number, clientHeight: number, scrollTop: number): boolean {
+	const position = scrollHeight - scrollTop - clientHeight;
+	return position <= borderOffset;
 }
