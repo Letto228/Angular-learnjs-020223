130b27f3ef72fe3787cddbb5d7501713ef94488b
Letto228 <54375830+Letto228@users.noreply.github.com>
2023-05-02 17:07:01 +0300

    Revert "feat: add save filter to query params"

--- src/app/pages/products-list/filter/filter.component.ts
+++ src/app/pages/products-list/filter/filter.component.ts
@@ -4,39 +4,22 @@ import {
 	EventEmitter,
 	Input,
 	OnChanges,
-	OnInit,
 	Output,
 	SimpleChanges,
 } from '@angular/core';
-import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, FormGroupName, Validators } from '@angular/forms';
 import { IProductsFilter } from './products-filter.interface';
-import { Observable, map, takeUntil } from 'rxjs';
-import { DestroyService } from '../../../shared/destroy/destroy.service';
 
 @Component({
 	selector: 'app-filter',
 	templateUrl: './filter.component.html',
 	styleUrls: ['./filter.component.css'],
 	changeDetection: ChangeDetectionStrategy.OnPush,
-	providers: [DestroyService],
 })
-export class FilterComponent implements OnInit, OnChanges {
-	readonly filterForm = this.formBuilder.group({
-		name: ['', { validators: [Validators.minLength(3)] }],
-		brands: this.formBuilder.array<boolean>([]),
-		priceRange: this.formBuilder.group({
-			min: -1,
-			max: 0,
-		}),
-	});
-
+export class FilterComponent implements OnChanges {
 	@Input() brands!: string[] | null;
-	@Input() initialFilter!: IProductsFilter;
 
 	@Output() changeFilter = new EventEmitter<IProductsFilter>();
-
-	// Output by stream
-	// @Output() changeFilter = this.getFilterStream$();
 
 	// readonly filterForm = new FormGroup({
 	// 	name: new FormControl('', {validators: [Validators.minLength(3)]}),
@@ -47,19 +30,30 @@ export class FilterComponent implements OnInit, OnChanges {
 	// 	})
 	// });
 
-	constructor(
-		private readonly formBuilder: FormBuilder,
-		private readonly destroy$: DestroyService, // private readonly formGroupName: FormGroupName,
-	) {
+	readonly filterForm = this.formBuilder.group({
+		name: ['', { validators: [Validators.minLength(3)] }],
+		brands: this.formBuilder.array<boolean>([]),
+		priceRange: this.formBuilder.group({
+			min: -1,
+			max: 0,
+		}),
+	});
+
+	constructor(private readonly formBuilder: FormBuilder) // private readonly formGroupName: FormGroupName,
+	{
 		// this.formGroupName.control
-		// this.filterForm.valueChanges.subscribe(formValue => {
-		// 	const { brands, ...otherValues } = formValue;
-		// 	// console.log({
-		// 	// 	...otherValues,
-		// 	// 	brands: this.getPrandsList(brands as boolean[]),
-		// 	// })
-		// 	// this.changeFilter.emit()
-		// });
+
+		this.filterForm.valueChanges.subscribe(formValue => {
+			const { brands, ...otherValues } = formValue;
+
+			// console.log({
+			// 	...otherValues,
+			// 	brands: this.getPrandsList(brands as boolean[]),
+			// })
+
+			// this.changeFilter.emit()
+		});
+
 		// this.filterForm.get('name')?.valueChanges.subscribe(console.log);
 	}
 
@@ -69,59 +63,16 @@ export class FilterComponent implements OnInit, OnChanges {
 
 	ngOnChanges({ brands }: SimpleChanges): void {
 		if (brands) {
-			const initialFilterBrands = this.initialFilter.brands;
-			const brandsControls = this.brands
-				? this.brands.map(name => this.formBuilder.control(initialFilterBrands.includes(name)))
-				: [];
+			const brandsControls = this.brands ? this.brands.map(() => this.formBuilder.control(false)) : [];
 			const newBrandsFormArray = this.formBuilder.array(brandsControls);
 
 			this.filterForm.setControl('brands', newBrandsFormArray);
 		}
 	}
 
-	ngOnInit() {
-		this.initFilterValue();
-		this.listenChangeFilterForm();
-	}
-
 	onSubmit(formValue: unknown) {
 		// console.log(formValue);
 	}
-
-	private initFilterValue() {
-		const { name, priceRange } = this.initialFilter;
-
-		this.filterForm.patchValue({ name, priceRange });
-	}
-
-	private listenChangeFilterForm() {
-		this.filterForm.valueChanges
-			.pipe(
-				map(
-					({ brands, ...otherValues }) =>
-						({
-							...otherValues,
-							brands: this.getPrandsList(brands as boolean[]),
-						} as IProductsFilter),
-				),
-				takeUntil(this.destroy$),
-			)
-			.subscribe(filter => {
-				this.changeFilter.emit(filter);
-			});
-	}
-
-	// Output by stream
-	// private getFilterStream$(): Observable<IProductsFilter> {
-	// 	return this.filterForm.valueChanges
-	// 		.pipe(
-	// 			map(({ brands, ...otherValues }) => ({
-	// 				...otherValues,
-	// 				brands: this.getPrandsList(brands as boolean[]),
-	// 			}) as IProductsFilter),
-	// 			takeUntil(this.destroy$)
-	// 		)
-	// }
 
 	private getPrandsList(brandsActiveFlags: boolean[]): IProductsFilter['brands'] {
 		if (!this.brands) {
